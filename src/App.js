ee2e544205843f61ed7c2f737a9ce4f042f2ed34
Cole Fortner <colefortner@gmail.com>
2021-11-27 23:53:10 -0500

    Implement react-router and first route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Users from "./user/pages/Users";
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Users />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
 
 export default App;
