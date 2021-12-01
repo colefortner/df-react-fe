d1ffdc08ac1400fd23117ef05566ae9e2ce4cbf5
Cole Fortner <colefortner@gmail.com>
2021-11-30 22:43:20 -0500

    Created portal for the SideDrawer

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,7 +1,11 @@
+import ReactDOM from "react-dom";
+
 import styles from "./SideDrawer.module.css";
 
 const SideDrawer = (props) => {
-  return <aside class={styles["side-drawer"]}>{props.children}</aside>;
+  const content = <aside class={styles["side-drawer"]}>{props.children}</aside>;
+
+  return ReactDOM.createPortal(content, document.getElementById("drawer-hook"));
 };
 
 export default SideDrawer;
