b423f4d231c211ad0321d7f66ea3a21bfe9481c4
Cole Fortner <colefortner@gmail.com>
2021-12-05 22:53:10 -0500

    Add state management to our input with useReducer

--- src/businesses/pages/NewBusiness.js
+++ src/businesses/pages/NewBusiness.js
@@ -4,7 +4,13 @@ import Input from "../../shared/components/FormElements/Input";
 const NewBusiness = () => {
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Title" />
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Please enter a valid title"
+      />
     </form>
   );
 };
