909598e0b8ee9f56c60a81d7c70c8ae549aeecaf
Cole Fortner <colefortner@gmail.com>
2021-12-06 21:44:37 -0500

    Add useEffect to only fire the inputHandlers if input values or validitiy changes

--- src/businesses/pages/NewBusiness.js
+++ src/businesses/pages/NewBusiness.js
@@ -1,16 +1,34 @@
 import "./NewBusiness.css";
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE
+} from "../../shared/util/validators";
+import { useCallback } from "react";
 
 const NewBusiness = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="place-form">
       <Input
+        id="title"
         element="input"
         type="text"
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title"
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        rows="5"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)"
+        onInput={descriptionInputHandler}
       />
     </form>
   );
